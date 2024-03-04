0592f1f604fd25a7e77da4807f7a914232dc14a5
FuluZwivhuya <148289949+FuluZwivhuya@users.noreply.github.com>
2024-03-04 20:16:46 +0200

    undefined

---
+++ script.js
@@ -0,0 +1,23 @@
+let firstName = 'John';
+let age = 35;
+let hobby = 'Coding';
+
+function logTwice (a,b){
+  return a,b
+}
+
+logTwice (console.log((`Hello, ${firstName} (${age}). I love ${hobby}!`),
+         console.log((`Hello, ${firstName} (${age}). I love ${hobby}!`))));
+
+//OR
+
+/*function logTwice(){
+    console.log((`Hello, ${firstName} (${age}). I love ${hobby}!`))
+    console.log((`Hello, ${firstName} (${age}). I love ${hobby}!`))
+}
+
+function hobbyA (){
+    logTwice()
+}
+
+hobbyA()*/  
